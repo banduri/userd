a66a24e24b33789a80d9011cdb09da7369cb8daa
Alexander Kasper <foo@hallo.de>
2015-09-12 19:57:03 +0200

    adding nullable attribute

--- userd/models.py
+++ userd/models.py
@@ -1,33 +1,35 @@
 import sqlalchemy as sqa
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class User(sqa.Base):
     __tablename__ = 'user'
-    id = sqa.Column(sqa.Integer, primary_key=True,  autoincrement = True)
-    uid = sqa.Column(sqa.Integer)
-    uname = sqa.Column(sqa.String)
-    main_group = sqa.Column(sqa.Integer)
+    id = sqa.Column(sqa.Integer, primary_key = True,  nullable = False)
+    uid = sqa.Column(sqa.Integer, nullable = False)
+    uname = sqa.Column(sqa.String, nullable = False)
+    main_group = sqa.Column(sqa.Integer, nullable = False)
     #flags set('lock','perm','hidden','group_leader','male','adm','multiLogin'
-    password = sqa.Column(sqa.String)
-    pw_md5 = sqa.Column(sqa.String)
-    pw_lan = sqa.Column(sqa.String)
-    pw_nt = sqa.Column(sqa.String)
-    shell = sqa.Column(sqa.String)
-    home = sqa.Column(sqa.String)
-    last_host = sqa.Column(sqa.String)
-    name1 = sqa.Column(sqa.String)
-    name2 = sqa.Column(sqa.String)
-    department = sqa.Column(sqa.String)
-    expires = sqa.Column(sqa.Integer)
-    last_login = sqa.Column(sqa.Integer)
-    creator = sqa.Column(sqa.Integer)
-    creation_time = sqa.Column(sqa.Integer)
-    updater = sqa.Column(sqa.Integer)
-    update_time = sqa.Column(sqa.Integer)
-    lck_user = sqa.Column(sqa.Integer)
-    lck_time = sqa.Column(sqa.Integer)
-    lck_ip = sqa.Column(sqa.String)
-    note = sqa.Column(sqa.String)
+    password = sqa.Column(sqa.String, nullable = False)
+    pw_md5 = sqa.Column(sqa.String, nullable = True)
+    pw_lan = sqa.Column(sqa.String, nullable = True)
+    pw_nt = sqa.Column(sqa.String, nullable = True)
+    shell = sqa.Column(sqa.String, nullable = False)
+    home = sqa.Column(sqa.String, nullable = False)
+    last_host = sqa.Column(sqa.String, nullable = False)
+    name1 = sqa.Column(sqa.String, nullable = False)
+    name2 = sqa.Column(sqa.String, nullable = False)
+    department = sqa.Column(sqa.String, nullable = False)
+    expires = sqa.Column(sqa.Integer, nullable = False)
+    last_login = sqa.Column(sqa.Integer, nullable = False)
+    creator = sqa.Column(sqa.Integer, nullable = False)
+    creation_time = sqa.Column(sqa.Integer, nullable = False)
+    updater = sqa.Column(sqa.Integer, nullable = False)
+    update_time = sqa.Column(sqa.Integer, nullable = False)
+    lck_user = sqa.Column(sqa.Integer, nullable = False)
+    lck_time = sqa.Column(sqa.Integer, nullable = False)
+    lck_ip = sqa.Column(sqa.String, nullable = True)
+    note = sqa.Column(sqa.String, nullable = True)
 
     def __repr__(self):
         return "<User(name='%s', fullname='%s', password='%s')>" % (
@@ -35,29 +37,29 @@ class User(sqa.Base):
 
 class Group(sqa.Base):
     __tablename__ = 'ugroup'
-    id = sqa.Column(sqa.Integer,  primary_key = True,  autoincrement = True)
-    gid = sqa.Column(sqa.Integer)
-    gname = sqa.Column(sqa.String)
+    id = sqa.Column(sqa.Integer,  primary_key = True, nullable = False)
+    gid = sqa.Column(sqa.Integer, nullable = False)
+    gname = sqa.Column(sqa.String, nullable = False)
     #flags enum('normal','pseudo')
-    creator = sqa.Column(sqa.Integer)
-    creation_time = sqa.Column(sqa.Integer)
-    note = sqa.Column(sqa.String)
+    creator = sqa.Column(sqa.Integer, nullable = False)
+    creation_time = sqa.Column(sqa.Integer, nullable = False)
+    note = sqa.Column(sqa.String, nullable = True)
 
 class Addr(sqa.Base):
     __tablename__ = 'addr'
-    id = sqa.Column(sqa.Integer, primary_key = True, autoincrement = True)
-    id_user = sqa.Column(sqa.Integer)
-    name = sqa.Column(sqa.String)
-    country = sqa.Column(sqa.String)
-    city = sqa.Column(sqa.String)
-    zip_code = sqa.Column(sqa.String)
-    street_addr = sqa.Column(sqa.String)
-    zip_pobox = sqa.Column(sqa.String)
-    pobox = sqa.Column(sqa.String)
+    id = sqa.Column(sqa.Integer, primary_key = True, nullable = False)
+    id_user = sqa.Column(sqa.Integer, nullable = False)
+    name = sqa.Column(sqa.String, nullable = True)
+    country = sqa.Column(sqa.String, nullable = True)
+    city = sqa.Column(sqa.String, nullable = True)
+    zip_code = sqa.Column(sqa.String, nullable = True)
+    street_addr = sqa.Column(sqa.String, nullable = True)
+    zip_pobox = sqa.Column(sqa.String, nullable = True)
+    pobox = sqa.Column(sqa.String, nullable = True)
 
 class Phone(sqa.Base):
     __tablename__ = 'phone'
-    id = sqa.Column(sqa.Integer, primary_key = True, autoincrement = True)
-    id_user = sqa.Column(sqa.Integer)
-    name = sqa.Column(sqa.String)
-    phone = sqa.Column(sqa.String)
+    id = sqa.Column(sqa.Integer, primary_key = True, nullable = False)
+    id_user = sqa.Column(sqa.Integer, nullable = False)
+    name = sqa.Column(sqa.String, nullable = False)
+    phone = sqa.Column(sqa.String, nullable = False)
