ac42bf352e4c2554683912ad126ba074364a1ed5
Alexander Kasper <foo@hallo.de>
2015-09-15 01:50:29 +0200

    added /static
    sending groups to template

--- userd/controllers.py
+++ userd/controllers.py
@@ -5,7 +5,7 @@ import os
 
 from jinja2 import Environment, PackageLoader
 
-workingin="/home/asklepios/eric-workspace/userd"
+workingin="/home/asklepios/eric-workspace/userd/userd"
 templatedir=workingin+"/views"
 
 os.chdir(workingin)
@@ -13,7 +13,7 @@ os.chdir(workingin)
 
 import sqlalchemy as sqa
 from sqlalchemy.orm import sessionmaker
-from models import User
+from models import User, Group
 
 env = Environment(loader=PackageLoader('views', 'templates'))
 
@@ -29,8 +29,34 @@ class Root(object):
     @cherrypy.expose
     def index(self):
         our_user = session.query(User).filter_by(uname='ak').first()
+        ingroups = session.query(Group).filter(Group.gname.like("in%")).order_by(Group.gname)
+        abtgroups = session.query(Group).filter(Group.gname.like("abt_%")).order_by(Group.gname)
+        gremgroups = session.query(Group).filter(Group.gname.like("grem_%")).order_by(Group.gname)
+        resgroups = session.query(Group).filter(Group.gname.like("res_%")).order_by(Group.gname)
+        irgroups = session.query(Group).filter(Group.gname.like("ir_%")).order_by(Group.gname)
+        amqpgroups = session.query(Group).filter(Group.gname.like("amqp_%")).order_by(Group.gname)
+        other = session.query(Group).filter(~Group.gname.like("in%"),
+                                            ~Group.gname.like("abt_%"),
+                                            ~Group.gname.like("grem_%"),
+                                            ~Group.gname.like("res_%"),
+                                            ~Group.gname.like("ir%"),
+                                            ~Group.gname.like("amqp_%")).order_by(Group.gname)
+        groups=[('Ortsgruppen', ingroups),
+                ('Abteilungen', abtgroups),
+                ('Gremien', gremgroups),
+                ('Ressors', resgroups),
+                ('Interred', irgroups),
+                ('AMQP-Notifier', amqpgroups),
+                ('Andere', other)]
+
         template_index=env.get_template('base.tmpl')
-        return template_index.render(user=our_user)
+        return template_index.render(user=our_user, groups=groups)
+
 
 if __name__ == '__main__':
-   cherrypy.quickstart(Root(), '/')
+    conf = {'/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': os.path.join(workingin, 'static')
+            }}
+    print(os.path.join(workingin, 'static'))
+    cherrypy.quickstart(Root(), '/', config=conf)
