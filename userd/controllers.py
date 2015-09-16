468243dc0ef4291f59ae3e60db0418b6c43b04ee
Alexander Kasper <foo@hallo.de>
2015-09-16 22:53:23 +0200

    moving session insite methods - controlerpart of userlistsplit

--- userd/controllers.py
+++ userd/controllers.py
@@ -18,16 +18,15 @@ from models import User, Group
 env = Environment(loader=PackageLoader('views', 'templates'))
 
 
-engine = sqa.create_engine('mysql+mysqlconnector://root@localhost/user?charset=utf8')
+engine = sqa.create_engine('mysql+mysqlconnector://root@localhost/user?charset=utf8', echo=True)
 connection = engine.connect()
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 class Root(object):
 
     @cherrypy.expose
     def adminuser(self, username):
+        Session = sessionmaker(bind=engine)
+        session = Session()
         our_user = session.query(User).filter_by(uname=username).first()
         # special case informix: it's not a locationgroup
         ingroups = session.query(Group).filter(Group.gname.like("in%"), ~(Group.gname=='informix')).order_by(Group.gname)
@@ -42,13 +41,13 @@ class Root(object):
                                             ~Group.gname.like("res_%"),
                                             ~Group.gname.like("ir%"),
                                             ~Group.gname.like("amqp_%")).order_by(Group.gname)
-        groups=[('Ortsgruppen', ingroups),
-                ('Abteilungen', abtgroups),
-                ('Gremien', gremgroups),
-                ('Ressors', resgroups),
-                ('Interred', irgroups),
-                ('AMQP-Notifier', amqpgroups),
-                ('Andere', other)]
+        groups = [  ('Ortsgruppen', ingroups),
+                    ('Abteilungen', abtgroups),
+                    ('Gremien', gremgroups),
+                    ('Ressors', resgroups),
+                    ('Interred', irgroups),
+                    ('AMQP-Notifier', amqpgroups),
+                    ('Andere', other)]
 
 
 
@@ -57,14 +56,39 @@ class Root(object):
 
     @cherrypy.expose
     def group(self, groupname):
+        Session = sessionmaker(bind=engine)
+        session = Session()
         our_group = session.query(Group).filter_by(gname=groupname).first()
-        users = session.query(User)
+        allusers = session.query(User).order_by(User.uname)
+        # filter by flag this is ugly - but there is no support for "FIND_IN_SET('value',set_col)" in sqlalchemy
+        # selecting all 'lock'ed users
+        lockedusers = []
+        for user in allusers :
+            if 'lock' in user.flags:
+                lockedusers.append(user)
+        # selecting all 'hidden' users
+        hiddenusers = []
+        for user in allusers:
+            if 'hidden' in user.flags:
+                hiddenusers.append(user)
+        # selecting all users not 'hidden' and not 'lock'ed
+        activeusers = []
+        for user in allusers :
+            if not 'hidden' in user.flags and not 'lock' in user.flags:
+                activeusers.append(user)
+        users = [   ("aktive", activeusers),
+                    ("gesperrte", lockedusers),
+                    ("versteckte", hiddenusers)]
+        print(list(hiddenusers))
+
         template_index=env.get_template('group.tmpl')
         return template_index.render(group=our_group, users=users)
 
 
     @cherrypy.expose
     def user(self):
+        Session = sessionmaker(bind=engine)
+        session = Session()
         our_user = session.query(User).filter_by(uname='ak').first()
         template_index=env.get_template('user.tmpl')
         return template_index.render(user=our_user)
